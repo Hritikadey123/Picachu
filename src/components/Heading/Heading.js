b57680f84f8d04c9c25fad12dd41492a09ba20e1
Pratik Das <pratik14901@gmail.com>
2023-05-17 15:37:50 +0530

    firebase deployed

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -2,8 +2,13 @@ import './Heading.css';
 
 export default function Heading() {
 	return (
-		<h1 className="heading">
-            P<i></i>kédex
-		</h1>
+		<>
+			<h1 className="heading">
+				P<i></i>kédex
+			</h1>
+			<h4 className="info">
+				Made by Pratik Das
+			</h4>
+		</>
 	);
 }
